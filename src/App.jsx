c8f5e270bf368fe531a6e528e9b2fc028c645e5f
Martin-J-Larre <martinjlarre@gmail.com>
2023-08-29 23:39:19 -0400

    #3 Pages and layout for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Product } from "./pages/Product";
-import { HomePage } from "./pages/HomePage";
-import { PricingPage } from "./pages/PricingPage";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
 import { NoFoundPage } from "./pages/NoFoundPage";
-import { AppLayout } from "./pages/AppLayout";
+import { AppLayout } from "./layout/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/pricing" element={<Pricing />} />
         <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<NoFoundPage />} />
       </Routes>
